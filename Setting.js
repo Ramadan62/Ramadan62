aac3aac04af35b6af8feb45b5a07a58c0d14a707
Ramadan62 <ramadaniyansah77.7@gmail.com>
2025-06-09 12:01:26 +0700

    Setting.js

---
+++ Setting.js
@@ -0,0 +1,32 @@
+//Tahap uji coba cuy jadi jangan heran
+//Mohon bantuannya iya karena gua masih pemula
+
+global.botname = `Jinshi` //url bot name
+global.ownernumber = `6287870200416` //url owner number
+global.ownername = `Ramadhan62` //url owner name
+global.wagc = "https://chat.whatsapp.com/C3d9TvKDaXiJzlHvA54V0V"
+global.themeemoji = '💌'
+global.wm = "Jinshi"
+global.packname = "stiker by Madan"
+global.author = "Ramadhan62"
+
+// Require
+const fs = require('fs')
+const chalk = require('chalk')
+const {
+   indonesia
+} = require("./File/language");
+
+global.imgUrl = "https://files.catbox.moe/4j9d8x.jpg"
+global.thumbnailUrl = "https://files.catbox.moe/tllweb.jpg"
+
+global.mess = {
+    tes: '_hello ada yang bis jinshi bantu ketik !menu_',
+    success: 'succes',
+    admin: '_*Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !*_',
+    botAdmin: '_*Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !*_',
+    owner: '_*Perintah Ini Hanya Bisa Digunakan Oleh Owner !*_',
+    group: '_*Perintah Ini Hanya Bisa Digunakan Di Group Chat !*_',
+    private: '_Perintah Ini Hanya Bisa Digunakan Di Private Chat !*_',
+    wait: '_* Sedang Di Proses !*_',
+}
